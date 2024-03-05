8ee38f7dfe927d9684c1767d2a72e30531d8cbae
Kattria Nanda Petma <kattriananda@gmail.com>
2024-03-06 00:17:13 +0700

    Penambahan Pilihan

--- gen-20-java-basic-collecction/src/Main.java
+++ gen-20-java-basic-collecction/src/Main.java
@@ -1,28 +1,59 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList <String> namaBarang = new ArrayList<>();
+        ArrayList<String> namaBarang = new ArrayList<>();
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Masukkan Nama Barang (ketik selesai \njika penginputan selesai)");
+        System.out.println("Masukkan Nama Barang (ketik selesai jika penginputan selesai)");
         String inNamaBarang = input.nextLine();
 
-        while (!inNamaBarang.equalsIgnoreCase("selesai")){
+        while (!inNamaBarang.equalsIgnoreCase("selesai")) {
             namaBarang.add(inNamaBarang);
             inNamaBarang = input.nextLine();
         }
 
-        System.out.println("\nDaftar Nama barang");
+            System.out.println("Apa yang ingin anda lakukan dengan data ini?");
+            System.out.println("1. Mengetahui Jumlah Nama Barang yang Telah di Input");
+            System.out.println("2. Menampilkan Daftar Nama Barang Yang Telah Di input");
+            System.out.println("3. Menampilkan Nama Barang Berdasarkan Index");
+            System.out.println("4. Konversi Data ke ArrayList ke LinkedList dan Tampilkan");
+            System.out.println("Msukkan Pilihan = ");
+            int n = input.nextInt();
 
-        if (namaBarang.isEmpty()) {
-            System.out.println("Tidak ada barang yang dimasukkan");
-        }else {
-            for (String barang : namaBarang){
-                System.out.println("- "+barang);
+            switch (n) {
+                case 1:
+                    sumInputNB(namaBarang);
+                    break;
+                case 2:
+                    daftarNB(namaBarang);
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+
             }
-        }
 
     }
+
+    //cek jumlah input
+    public static void sumInputNB(ArrayList<String> nBarang) {
+        System.out.print("Jumlah Nama Barang Yang telah di Input = " + nBarang.size());
+    }
+
+    //Daftar Nama barang
+    public static void daftarNB (ArrayList<String> daftarNB) {
+        System.out.println("\nDaftar Nama barang");
+        if (daftarNB.isEmpty()) {
+            System.out.println("Tidak ada barang yang dimasukkan");
+        } else {
+            for (String barang : daftarNB) {
+                System.out.println("- " + barang);
+            }
+        }
+    }
+
 }
