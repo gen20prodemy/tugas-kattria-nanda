3f0fc9647b0d57280f3c7da462a50a0f88fb7bcb
Kattria Nanda <kattriananda@gmail.com>
2024-04-05 23:01:00 +0700

    Pemisahan Security

---
+++ gen-20-java-springboot-pos-security/src/test/java/com/example/gen20javaspringbootpos/Gen20JavaSpringbootPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gen20javaspringbootpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gen20JavaSpringbootPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
